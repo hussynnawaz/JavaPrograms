3a35314dd17c7651f78342da1597dc629f4ac6ab
hussynnawaz <hussynnawaz>
2022-11-09 22:48:29 +0500

    Sales Program added

---
+++ Sales/salestest.java
@@ -0,0 +1,11 @@
+public class TwoDimensionalSalesTest
+{
+   // main begins execution here
+   public static void main( String args[])
+   {
+      TwoDimensionalSales mySales = new TwoDimensionalSales();
+	  mySales.Sales();
+	  
+   }// end main
+
+}// end class TwoDimensionalSalesTest
