b8c59431b107ffc74d2ddf48296b4b3ce5a8f567
hussynnawaz <hussyn.nawaz@gmail.com>
2022-11-15 22:59:40 +0500

    Arrays

---
+++ Mid Term/Arr.java
@@ -0,0 +1,16 @@
+
+package fa19_bcs_183;
+
+
+public class Arr {
+    public static void main(String[] args)
+    {
+  
+int [] Array = {24,19,14,45,12};
+        String[] oddArray = null;
+System.out.println("Array[0] = " + oddArray[0]);
+        System.out.println("oddArray[1] = " + oddArray[1]);
+        System.out.println("oddArray[2] = " + oddArray[2]);
+        System.out.println("oddArray[3] = " + oddArray[3]);
+}
+}
