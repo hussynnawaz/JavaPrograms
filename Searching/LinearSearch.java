96088cf0f1c42fe9d5c8eba1e7604cb8e5c9dd85
hussynnawaz <hussyn.nawaz@gmail.com>
2022-11-16 00:07:21 +0500

    Linear Search

---
+++ Searching/LinearSearch.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package search;
+
+/**
+ *
+ * @author fa19-bcs-074
+ */
+class LinearSearch {
+     public static int search(int arr[], int x)
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] == x)
+                return i;
+        }
+        return -1;
+    }
+}
---
+++ Searching/LinearSearchTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package search;
+
+/**
+ *
+ * @author fa19-bcs-074
+ */
+public class LinearSearchTest {
+    // Driver code
+    public static void main(String[] args)
+    {
+        LinearSearch ob = new LinearSearch();
+        int arr[] = { 1, 2, 3, 4, 5 };
+        int search_element = 5;
+       
+        // Function call
+        LinearSearch(arr,search_element);
+    }
+}
