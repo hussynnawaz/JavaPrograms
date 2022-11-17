ac9cd221dcd415d2b553bc213300db4f1fd9e556
hussynnawaz <hussyn.nawaz@gmail.com>
2022-11-17 20:13:45 +0500

    Sorting Updated

---
+++ Sorting/QuickSortTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package search;
+
+/**
+ *
+ * @author fa19-bcs-074
+ */
+public class QuickSortTest {
+    // Driver Code
+public static void main(String[] args)
+{
+	int[] arr = { 10, 7, 8, 9, 1, 5 };
+	int n = arr.length;
+	QuickSort ob = new QuickSort 
+	QuickSort(arr, 0, n - 1);
+	System.out.println("Sorted array: ");
+	printArray(arr, n);
+}
+}
