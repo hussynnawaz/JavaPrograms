051921abb51bcc90cff4d4c9f7940ee57e67c7ea
hussynnawaz <hussyn.nawaz@gmail.com>
2022-11-19 00:13:32 +0500

    Interpolation Search

---
+++ Other Programs/InterpolationSearchTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AllPrograms;
+
+import static AllPrograms.InterpolationSearch.InterpolationSearch;
+
+/**
+ *
+ * @author Admin
+ */
+public class InterpolationSearchTest {
+    
+    // Driver Code
+	public static void main(String[] args)
+	{
+
+		// Array of items on which search will
+		// be conducted.
+		int arr[] = { 12, 13, 16, 18, 19, 20, 21,
+					22, 23, 24, 33, 35, 42, 47 };
+
+		int n = arr.length;
+
+		// Element to be searched
+		int x = 18;
+		int index = InterpolationSearch(arr, 0, n - 1, x);
+
+		// If element was found
+		if (index != -1)
+			System.out.println("Element found at index "
+							+ index);
+		else
+			System.out.println("Element not found.");
+	}
+}
